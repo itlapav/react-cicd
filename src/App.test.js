1c146ac7cf732bc82e22032dc3e2e3a8fd7deb3a
itlapav <itlap983@gmail.com>
2025-07-13 14:04:28 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders welcome message', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to Pavan Itla/i);
